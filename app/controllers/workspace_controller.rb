bef2f480b4eca9152d5f4f2a0e8bde37585245db
Scott Adams <scott_adams080291@hotmail.com>
2023-03-15 17:06:26 +0000

    fix(workspace-controller): simplified with include and allowed for all workspace parameters to get passed to frontend

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -3,15 +3,12 @@ class WorkspaceController < ApplicationController
   
   def show
     workspace = Workspace.includes(:postit_mappings).where(id: params[:id])
-    postits = []
     status = :no_content
     if Workspace.includes(:postit_mappings).where(id: params[:id]).any?
       workspace = Workspace.find_by_id(params[:id])
-      mappings = workspace.postit_mappings.includes(:postit)
-      mappings.map { | mapping | postits.append(mapping.postit) }
       status = :ok
     end
-    render json: postits, :status => status
+    render json: workspace.to_json(:include => :postits), :status => status
   end
 
   def create
